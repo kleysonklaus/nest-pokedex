736d9cc1964e69766d42c71208c17804f4375efa
kleyson klaus <kleyson.zur44@gmail.com>
2023-08-19 21:43:11 -0500

    se añade el schema de pokemon como referencia de modulo para la base de datos

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,23 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+
+    // id: string // mongo me lo da (automatico)
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    name: string;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    no: number;
+}
+
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
