d4300aac08dc308e088efc7316a1485cad49198d
kleyson klaus <kleyson.zur44@gmail.com>
2023-08-20 15:13:25 -0500

    se crea un nuevo validador de mongo id

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -13,7 +14,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
 
-    PokemonModule
+    PokemonModule,
+
+    CommonModule
   ],
   controllers: []
 })
