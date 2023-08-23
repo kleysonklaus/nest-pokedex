7f80677f5a56c903bbc2532aa11ec7bdc299313a
kleyson klaus <kleyson.zur44@gmail.com>
2023-08-22 23:40:40 -0500

    se elimina comentarios

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { PokemonService } from 'src/pokemon/pokemon.service';
-import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+// import { PokemonService } from 'src/pokemon/pokemon.service';
+// import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokemonModule } from 'src/pokemon/pokemon.module';
 import { CommonModule } from 'src/common/common.module';
 
